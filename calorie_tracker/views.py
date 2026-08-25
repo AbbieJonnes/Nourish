5f4cfbdbfa26459d43c35e13fa3f3f0159ae557f
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-08-25 23:38:04 +0300

    fix:update views.py by adding the dashboard function and authentication

--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
+from .models import FoodItem
 
 
 def register(request):
@@ -17,4 +19,18 @@ def register(request):
 
     return render(request, 'calorie_tracker/register.html', {
         'form': form
+    })
+
+
+@login_required
+def dashboard(request):
+    foods = FoodItem.objects.filter(
+        user=request.user
+    ).order_by('-date_added', '-id')
+
+    total_calories = sum(food.calories for food in foods)
+
+    return render(request, 'calorie_tracker/dashboard.html', {
+        'foods': foods,
+        'total_calories': total_calories,
     })
