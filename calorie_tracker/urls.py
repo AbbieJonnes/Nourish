fd527cb8baf858349e32ab11b809d920b1481efb
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-08-26 00:30:39 +0300

    update:update the urls.py by adding the delete food path

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('add-food/', views.add_food, name='add_food'),
+    path('delete-food/<int:food_id>/', views.delete_food, name='delete_food'),
     path('', views.dashboard, name='dashboard'),
 ]
