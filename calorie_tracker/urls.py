ded8fd92824024b0f3e06f138eb61580a6a37603
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-08-26 00:19:40 +0300

    update:update urls.py by adding the add_food path

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('add-food/', views.add_food, name='add_food'),
     path('', views.dashboard, name='dashboard'),
 ]
