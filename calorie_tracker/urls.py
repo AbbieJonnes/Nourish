1b7418cc5c5516b8d9198849ac092d19111c2e1b
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-08-26 00:39:04 +0300

    update urls.py by adding the reset-day path

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('add-food/', views.add_food, name='add_food'),
     path('delete-food/<int:food_id>/', views.delete_food, name='delete_food'),
+    path('reset-day/', views.reset_day, name='reset_day'),
     path('', views.dashboard, name='dashboard'),
 ]
